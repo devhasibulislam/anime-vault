a16699fbc6d4fed34dd5f083a69b5dc971ef9cd5
devhasibulislam <devhasibulislam@gmail.com>
2023-12-02 13:11:37 +0600

    load more dynamic component init

---
+++ components/LoadMore.tsx
@@ -0,0 +1,77 @@
+/**
+ * Title: Write a program using JavaScript on LoadMore
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 02, December 2023
+ */
+
+"use client";
+
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+
+import { fetchAnime } from "../app/action";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2;
+
+function LoadMore() {
+  const { ref, inView } = useInView();
+
+  const [data, setData] = useState<AnimeProp[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (inView) {
+      setIsLoading(true);
+      // Add a delay of 500 milliseconds
+      const delay = 500;
+
+      const timeoutId = setTimeout(() => {
+        fetchAnime(page).then((res) => {
+          setData([...data, ...res]);
+          page++;
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      // Clear the timeout if the component is unmounted or inView becomes false
+      return () => clearTimeout(timeoutId);
+    }
+  }, [inView, data, isLoading]);
+
+  return (
+    <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      <section className="flex justify-center items-center w-full">
+        <div ref={ref}>
+          {inView && isLoading && (
+            <Image
+              src="./spinner.svg"
+              alt="spinner"
+              width={56}
+              height={56}
+              className="object-contain"
+            />
+          )}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default LoadMore;
