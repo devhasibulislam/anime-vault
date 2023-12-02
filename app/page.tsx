b050d9f861593d41c8dcbbf97b3b0acef0a24eb1
devhasibulislam <devhasibulislam@gmail.com>
2023-12-02 13:12:16 +0600

    home page init

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+/**
+ * Title: Write a program using JavaScript on Home
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 02, December 2023
+ */
+
+import { fetchAnime } from "./action";
+
+import LoadMore from "../components/LoadMore";
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+async function Home() {
+  const data = await fetchAnime(1);
+
+  return (
+    <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
+      <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
+
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      <LoadMore />
+    </main>
+  );
+}
+
+export default Home;
